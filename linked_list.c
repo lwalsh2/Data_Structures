f038a8ca4bed5b188d8156f11c38b693ef74bd22
lwalsh2 <lwalsh2@hawk.iit.edu>
2021-07-13 11:55:44 -0400

    Revert "Singly linked list"

--- linked_list.c
+++ linked_list.c
@@ -1,5 +1,5 @@
 /*
- * Singly Linked List Operations
+ * Linked list Operations
  * By: Liam P. Walsh
 */
 
@@ -151,33 +151,21 @@ int main()
   linked_list * list = calloc(1, sizeof(linked_list));
 	list->head = create_node(3);
   print_list(list);
-	if (!append_back_node(create_node(1), list)) {
-		printf("Append Back failed.\n");
-	}
+	append_back_node(create_node(1), list);
   print_list(list);
-	if (!append_back_node(create_node(2), list)) {
-		printf("Append Back failed.\n");
-	}
+	append_back_node(create_node(2), list);
   print_list(list);
-	if (!append_back_node(create_node(3), list)) {
-		printf("Append Back failed.\n");
-	}
+	append_back_node(create_node(2), list);
   print_list(list);
-	if (!append_front_node(create_node(1), list)) {
-		printf("Append Front failed.\n");
-	}
+	append_front_node(create_node(1), list);
   print_list(list);
 
-	printf("Removing nodes with value 1:\n");
-	if (!search_and_destroy_node(list, 1)) {
-		printf("Search and Destroy failed.\n");
-	}
+	printf("Removing middle node:\n");
+	search_and_destroy_node(list, 1);
 	print_list(list);
 
 	printf("Deleting Linked list:\n");
-	if (!empty_list(list)) {
-		printf("Empty List failed.\n");
-	}
+	empty_list(list);
 	print_list(list);
 	free(list);
   return 0;
