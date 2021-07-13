86740e4e05a38e4849b360f6eba3f40c67ae3e72
lwalsh2 <lwalsh2@hawk.iit.edu>
2021-07-13 11:01:03 -0400

    Append to back updated. Comments added for understanding.

--- linked_list.c
+++ linked_list.c
@@ -1,7 +1,12 @@
+/*
+ * Linked list Operations
+ * By: Liam P. Walsh
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
-// Declare linked node struct
+// Declares linked node struct.
 typedef struct linked_node_ {
   // Data of linked list node
   int number;
@@ -9,31 +14,41 @@ typedef struct linked_node_ {
   struct linked_node_ * node;
 } linked_node;
 
-// Declare linked list struct (hold first node)
+// Declares linked list struct. (hold first node)
 typedef struct linked_list_ {
   // The first node of the list
   linked_node * head;
 } linked_list;
 
+// Allocates node with the given data, and returns address.
 linked_node * create_node(int number) {
+	// Allocate data for node, setting values to NULL/0.
 	linked_node * node = calloc(1, sizeof(node));
+	// Set the node's data
 	node->number = number;
 	return node;
 }
 
+// Allocates list with the given head, and returns address.
 linked_list * create_list(linked_node * head) {
+	// Allocate list.
 	linked_list * list = malloc(sizeof(linked_list));
+	// Set the head of the list.
 	list->head = head;
 	return list;
 }
 
+// Append node to front of list. (Replaces head node)
 int append_front_node(linked_node * new, linked_list * list) {
+	// Makes sure there is a list and node to append.
 	if (!list || !new) {
 		return 0;
 	}
+	// If the list is empty, sets the node as head.
 	if (!list->head) {
 		list->head = new;
 	}
+	// Replaces the current head of the list.
 	else {
 		new->node = list->head;
 		list->head = new;
@@ -41,13 +56,17 @@ int append_front_node(linked_node * new, linked_list * list) {
 	return 1;
 }
 
+// Append node to end of list. (Parses for tail)
 int append_back_node(linked_node * new, linked_list * list) {
+	// Makes sure there is a list and node to append.
 	if (!list || !new) {
 		return 0;
 	}
+	// If the list is empty, sets the node as head.
 	if (!list->head) {
 		list->head = new;
 	}
+	// Finds the end of the list, and appends to the end.
 	else {
 		linked_node *node = list->head;
 		while (node) {
@@ -55,36 +74,44 @@ int append_back_node(linked_node * new, linked_list * list) {
 				node->node = new;
 				break;
 			}
+			node = node->node;
 		}
 	}
-	return 0;
+	return 1;
 }
 
+// Recursively frees nodes called by the node pointer.
 void free_node(linked_node * node) {
-	if (!node) {
-		return;
-	}
+	// Recursively calls to the end of the list.
 	if (node->node) {
 		free_node(node->node);
 	}
+	// Frees nodes, starting from the end of the list.
 	free(node);
 }
 
+// Frees nodes and sets head to NULL.
 int empty_list(linked_list * list) {
+	// Verify there's a list
 	if (!list) {
 		return 0;
 	}
+	// Free any nodes attached to the list.
 	if (list->head) {
 		free_node(list->head);
 	}
+	// Empties list by setting to NULL.
 	list->head = NULL;
 	return 1;
 }
 
+// Find and replace all instances of given key within given list.
 int search_and_destroy_node(linked_list * list, int key) {
+	// Verify there's a list and a node to parse.
 	if (!list || !list->head) {
 		return 0;
 	}
+	// Sets the current node to parse, and a temporary node to free.
 	linked_node *node = list->head;
 	linked_node *temp = NULL;
 	while (node->node) {
@@ -92,10 +119,10 @@ int search_and_destroy_node(linked_list * list, int key) {
 			temp = node->node;
 			node->node = node->node->node;
 			free(temp);
-			return 1;
 		}
 		node = node->node;
 	}
+	// Loop checks all but the head.
 	if (list->head->number == key) {
 		temp = list->head;
 		list->head = list->head->node;
@@ -104,7 +131,7 @@ int search_and_destroy_node(linked_list * list, int key) {
 	return 1;
 }
 
-
+// Parses list and prints node data.
 void print_list(linked_list * list) {
 	printf("Printing List: \n");
 	if (!list || !list->head) {
@@ -122,11 +149,15 @@ int main()
 {
   printf("Creating Linked list:\n");
   linked_list * list = calloc(1, sizeof(linked_list));
-	list->head = create_node(1);
+	list->head = create_node(3);
+  print_list(list);
+	append_back_node(create_node(1), list);
   print_list(list);
 	append_back_node(create_node(2), list);
   print_list(list);
-	append_front_node(create_node(3), list);
+	append_back_node(create_node(2), list);
+  print_list(list);
+	append_front_node(create_node(1), list);
   print_list(list);
 
 	printf("Removing middle node:\n");
