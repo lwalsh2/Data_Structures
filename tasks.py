16554e2d096a0178e6db21fdd36495e8486948bf
lwalsh2 <walshliam55@gmail.com>
2025-06-29 20:37:29 +0000

    Fix naming conventions

--- tasks.py
+++ tasks.py
@@ -5,10 +5,10 @@ Tasks to run through invoke
 from invoke import task, context
 
 # The Data Structures that can be called:
-sort_methods = ['b_tree', 'binary_tree', 'double_linked_list',
-                'fibonacci_array', 'fibonacci_loop', 'fibonacci_rec',
-                'fibonacci_tree', 'hash_table', 'heap', 'queue',
-                'single_linked_list', 'stack']
+data_structures = ['b_tree', 'binary_tree', 'double_linked_list',
+                   'fibonacci_array', 'fibonacci_loop', 'fibonacci_rec',
+                   'fibonacci_tree', 'hash_table', 'heap', 'queue',
+                   'single_linked_list', 'stack']
 
 @task()
 def clean(cmd: context.Context) -> None:
@@ -31,22 +31,22 @@ def build(cmd: context.Context) -> None:
     """
     cmd.run('mkdir bin;cmake -B bin -S .;cd bin;make')
 
-@task(build, help={'sort_type': 'Name of the sorting method to use. Use `all` to run each'})
-def run(cmd: context.Context, sort_type: str) -> None:
-    """Builds the project. Can be called with `invoke build <sort_type>`.
+@task(build, help={'structure_type': 'Name of the data structure to use. Use `all` to run each'})
+def run(cmd: context.Context, structure_type: str) -> None:
+    """Builds the project. Can be called with `invoke build <structure_type>`.
     For example, using `invoke build all` runs each, while
     `invoke build bubble` only runs bubble sort.
     Args:
         cmd (context.Context): Context invoke passes to run commands
-        sort_type (str): The sorting method to run, or `all`
+        structure_type (str): The data structure to run, or `all`
     Returns:
         None
     """
-    sort_lower = sort_type.lower()
-    if sort_lower == 'all':
-        for sort_method in sort_methods:
+    structure_lower = structure_type.lower()
+    if structure_lower == 'all':
+        for sort_method in data_structures:
             cmd.run(f"./bin/src/{sort_method}")
-    elif sort_lower in sort_methods:
-        cmd.run(f"./bin/src/{sort_lower}")
+    elif structure_lower in data_structures:
+        cmd.run(f"./bin/src/{structure_lower}")
     else:
-        print(f"Sort Type '{sort_type}' is not implemented")
+        print(f"Sort Type '{structure_type}' is not implemented")
